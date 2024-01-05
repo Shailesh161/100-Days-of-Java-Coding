580dffc0aa5c5b60036d2851f75e7ebed80db100
Shailesh <112454440+Shailesh161@users.noreply.github.com>
2024-01-05 23:47:00 +0530

    Create Day 1 : Number_Positive_negative

---
+++ Day 1 : Number_Positive_negative.java
@@ -0,0 +1,26 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int number = 5;
+
+     //Condition to check if the number is negative or positive
+    if (number >= 0)
+    {
+        if (number == 0)
+        {
+            System.out.println ("Zero");
+        }
+        else
+        {
+            System.out.println ("The number is positive");
+        }
+    }
+
+    else
+    {
+        System.out.println ("The number is negative");
+    }
+
+  }
+}
