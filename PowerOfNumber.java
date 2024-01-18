a26bdb62447c879ee9933ec38f6dd9e5e74c79b0
Shailesh <112454440+Shailesh161@users.noreply.github.com>
2024-01-18 20:49:12 +0530

    Create PowerOfNumber.java

---
+++ PowerOfNumber.java
@@ -0,0 +1,15 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int base = 5, x = 3;
+    System.out.println ("Required Power is " + power (base, x));
+  }
+  //Recursive Function
+  static int power (int base, int x)
+  {
+    if (x == 0)     //Base Condition
+      return 1;
+    return (base * power (base, x - 1));
+  }
+}
